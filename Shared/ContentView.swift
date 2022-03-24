503c85d84cbe5ba70afe8ff7aa04309858705d18
Arvios <xeniyabubnova@yandex.ru>
2022-03-24 14:51:13 +0300

    first commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 35 кабинет 9 on 17.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/_23App.swift
@@ -0,0 +1,17 @@
+//
+//  _23App.swift
+//  Shared
+//
+//  Created by 35 кабинет 9 on 17.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct _23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
